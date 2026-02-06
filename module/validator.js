60f025ddaea465434a6a1d2c2f7e01ce6162d7bb
PierreTDX <pierre.tondeux@gmail.com>
2026-02-06 12:13:04 +0100

    result test TDD : 4 failed, 2 passed refactor calculateAge(p) = 6 passed

--- module/validator.js
+++ module/validator.js
@@ -8,6 +8,18 @@ export function calculateAge(p) {
     if (!p) {
         throw new Error("missing param p")
     }
+    if (typeof p !== 'object') {
+        throw new Error("param p must be an object")
+    }
+    if (!p.birth) {
+        throw new Error("missing param p.birth")
+    }
+    if (!(p.birth instanceof Date)) {
+        throw new Error("p.birth must be a Date object")
+    }
+    if (isNaN(p.birth)) {
+        throw new Error("p.birth is an invalid Date")
+    }
     let dateDiff = new Date(Date.now() - p.birth.getTime())
     let age = Math.abs(dateDiff.getUTCFullYear() - 1970);
     return age;
